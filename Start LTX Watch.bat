9aa22f1239d2f39d2b9c387d838c962121a7da95
Orffyrus-Qc <102268366+Orffyrus-Qc@users.noreply.github.com>
2026-08-24 15:43:15 -0400

    fix: launch Studio feature interface from desktop

--- Start LTX Watch.bat
+++ Start LTX Watch.bat
@@ -1,6 +1,6 @@
 @echo off
-title LTX Watch
+title LTX Watch Studio
 cd /d "%~dp0"
-start "" powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 3; Start-Process 'http://localhost:3000'"
-npm run dev
+start "" powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 3; Start-Process 'http://localhost:3001'"
+npm run dev:studio
 pause
